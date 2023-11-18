f76ae308304276fdc2465fc526eba5613762d0d5
Jerry Kong <jerrykongmail@163.com>
2023-11-18 23:27:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,60 @@
 import http.client
 import requests
+import json
+from datetime import date
+import PySimpleGUI as ui
 
 API_key = '7vkjinjkwg6CCJY7qp32xMwWCzziNrwq'
 url = 'https://developers.cathaypacific.com/hackathon-apigw'
 
 
-def passengers(PassengerID):  # Search passengers based on passenger ID
-    conn = http.client.HTTPSConnection("developers.cathaypacific.com")
-    conn.request("GET", "/hackathon-apigw/airport/customers/" + PassengerID + "/details")
-    res = conn.getresponse()
-    data = res.read()
-    print(data.decode("utf-8"))
+# Search passengers based on passenger ID
+def passengers(PassengerID):
+    headers = {'apiKey': API_key, 'accept': 'application/json'}
+    res = requests.get(url + "/airport/customers/" + PassengerID + "/details", headers=headers)
+    data = res.content
+    print(data)
 
 
-def bagAllowance(PassengerID, SegmentID):  # Search passengers bag allowance based on passenger ID and segment ID
-    conn = http.client.HTTPSConnection("developers.cathaypacific.com")
-    conn.request("POST", "/hackathon-apigw/airport/customers/" + PassengerID + SegmentID + "/bagallowance")
-    res = conn.getresponse()
-    data = res.read()
-    print(data.decode("utf-8"))
+# Search passengers bag allowance based on passenger ID and segment ID
+def bagAllowance(PassengerID, SegmentID):
+    headers = {'apiKey': API_key, 'accept': 'application/json'}
+    res = requests.get(url + "/airport/customers/" + PassengerID + SegmentID + "/bagallowance", headers=headers)
+    data = res.content
+    print(data)
 
 
-def regulatoryRequirements(PassengerID,SegmentID):  # Search passengers regulatory requirements based on passenger ID and segment ID
-    conn = http.client.HTTPSConnection("developers.cathaypacific.com")
-    conn.request("POST", "/hackathon-apigw/airport/customers/" + PassengerID + SegmentID + "/regulatoryrequirements")
-    res = conn.getresponse()
-    data = res.read()
-    print(data.decode("utf-8"))
+# Search passengers regulatory requirements based on passenger ID and segment ID
+def regulatoryRequirements(PassengerID, SegmentID):
+    headers = {'apiKey': API_key, 'accept': 'application/json'}
+    res = requests.get(url + "/airport/customers/" + PassengerID + SegmentID + "/regulatoryrequirements",
+                       headers=headers)
+    data = res.content
+    print(data)
 
 
-def seatMap(PassengerID, SegmentID):  # Search passengers seatMap based on passenger ID and segment ID
-    conn = http.client.HTTPSConnection("developers.cathaypacific.com")
-    conn.request("POST",
-                 "/hackathon-apigw/airport/flights/" + PassengerID + SegmentID + "/seatmaps?fid=%3CSOME_STRING_VALUE%3E")
-    res = conn.getresponse()
-    data = res.read()
-    print(data.decode("utf-8"))
+# Search passengers seatMap based on passenger ID and segment ID
+def seatMap(PassengerID, SegmentID, FlightID):
+    headers = {'apiKey': API_key, 'accept': 'application/json'}
+    res = requests.get(url + "/airport/flights/" + PassengerID + SegmentID + "/seatmaps?fid=" + FlightID,
+                       headers=headers)
+    data = res.content
+    print(data)
 
 
-def flightDetails(FlightID):  # Search flight details based on flight number
-    conn = http.client.HTTPSConnection("developers.cathaypacific.com")
-    conn.request("POST", "/hackathon-apigw/airport/flights?fid=" + FlightID)
-    res = conn.getresponse()
-    data = res.read()
-    print(data.decode("utf-8"))
+# Search flight details based on flight number
+def flightDetails(FlightID):
+    headers = {'apiKey': API_key, 'accept': 'application/json'}
+    res = requests.get(url + "/airport/flights?fid=" + FlightID, headers=headers)
+    data = res.content
+    print(data)
 
 
 def main():
     print("Welcome to Cathay SwiftServe AI System made by Team_068: Cathay skyline")
+    print("Today is :", date.today())
     print("Connecting to Cathay Network, please wait a second...")
-    headers = {'Authorization': API_key}
+    headers = {'apiKey': API_key}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         print("Connected successfully!")
@@ -63,8 +68,8 @@ def main():
         print("3.Search passengers regulatory requirements based on passenger ID and segment ID.")
         print("4.Search passengers seatMap based on passenger ID and segment ID.")
         print("5.Search flight details based on flight number.")
-        Reply = input("Input 1, 2, 3, 4 or 5 for one of the functions, otherwise automatically terminating: ")
-        match Reply:
+        reply = input("Input 1, 2, 3, 4 or 5 for one of the functions, otherwise automatically terminating: ")
+        match reply:
             case '1':
                 passengers(pidinput())
             case '2':
@@ -72,7 +77,7 @@ def main():
             case '3':
                 regulatoryRequirements(pidinput(), sidinput())
             case '4':
-                seatMap(pidinput(), sidinput())
+                seatMap(pidinput(), sidinput(), fidinput())
             case '5':
                 flightDetails(fidinput())
             case _:
@@ -80,22 +85,89 @@ def main():
         print("")
         input("Press any key to return to main table...")
         print("")
-    input("The program has already terminated!")
+    input("The program has terminated!")
 
 
 def pidinput():
-    PassengerID = input("Please input your Passenger ID: ")
-    return PassengerID
+    return input("Please input your Passenger ID: ")
 
 
 def sidinput():
-    SegmentID = input("Please input your Segment ID: ")
-    return SegmentID
+    return input("Please input your Segment ID: ")
 
 
 def fidinput():
-    FlightID = input("Please input your Flight ID: ")
-    return FlightID
+    return input("Please input your Flight ID: ")
+
+
+def preorderSystem(PassengerID):
+    passengerChoice = ""
+    days = int(getRemainingDays(getDate(PassengerID)))
+    foodList = getFoodList()
+    while days > 1:
+        passengerChoice = mainui()
+        conf = input("Please press 'Y' to save your choice until the ordering period is closing: ")
+        if conf == 'Y':
+            print("Your choice have been saved successfully!")
+        else:
+            print("Your choice have not been saved!")
+    return passengerChoice
+
+
+def getDate(PassengerID):
+    headers = {'apiKey': API_key, 'accept': 'application/json'}
+    res = requests.get(url + "/airport/customers/" + PassengerID + "/details", headers=headers)
+    data = res.content
+    with open(data) as d:
+        datadic = json.load(d)
+    datedFlightID = datadic['data']['segmentDeliveries']['collection']['ref'][-16:]
+    date0 = datadic['included']['segmentDeliveries'][datedFlightID]['flightSegment']['departure']['at'][:10]
+    return date0
+
+
+def getFoodList():
+    foodList = []
+    food1 = "food1"
+    food2 = "food2"
+    foodList.append(food1)
+    foodList.append(food2)
+    return foodList
+
+
+def getRemainingDays(reserveDate):
+    return (date(reserveDate[:4], reserveDate[5:7], reserveDate[-2:]) - date(date.today()[:4], date.today()[5:7],
+                                                                             date.today()[-2:])).days
+
+
+def button1(text):
+    return ui.B(text, pad=(2, 2), size=(12, 4), font=('Calibri', 24), button_color='black')
+
+
+def button2(text):
+    return ui.B(text, pad=(1, 1), size=(50, 4), font=('Calibri', 18), button_color='black')
+
+
+def mainui():
+    layout = [
+        [button1(i) for i in '123'],
+        [button2(i) for i in ['Confirm']]
+    ]
+    window = ui.Window('Cathay SwiftServe AI', layout)
+    x = -1
+    while True:
+        event, values = window.read()
+        print(event)
+        if event is None:
+            break
+        if event == '1':
+            x = 1
+        if event == '2':
+            x = 2
+        if event == '3':
+            x = 0
+        if event == 'Confirm':
+            break
+    return x
 
 
 main()
